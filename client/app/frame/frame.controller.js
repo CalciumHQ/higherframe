3f45a3853490a8c87a4d57c6fc4d403a3071fdce
Simon Robb <sajrobb@gmail.com>
2015-04-26 17:46:19 -0400

    Updated UI

--- client/app/frame/frame.controller.js
+++ client/app/frame/frame.controller.js
@@ -4,5 +4,29 @@ angular
   .module('siteApp')
   .controller('FrameCtrl', function ($scope) {
 
-    $scope.message = 'Hello';
+    $scope.components = [
+      {
+        name: 'iPhone',
+        tags: [
+          'container',
+          'apple',
+          'screen'
+        ]
+      },
+      {
+        name: 'iMac',
+        tags: [
+          'container',
+          'apple',
+          'screen'
+        ]
+      },
+      {
+        name: 'Generic monitor',
+        tags: [
+          'container',
+          'screen'
+        ]
+      }
+    ];
   });
